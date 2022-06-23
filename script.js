2c0e060dd3976b1aa4c80892116891bf25df6d0b
Randie <srandall1213@gmail.com>
2022-06-23 13:55:00 -0600

    added alert/prompt/confirm

---
+++ script.js
@@ -0,0 +1,17 @@
+alert("Let's play Rock, Paper, Scissors!");
+
+prompt("Please enter R, P, or S to signify your choice of rock, paper, or scissors.");
+
+confirm("Random automated response R, P, or S");
+
+confirm("You Win!");
+
+confirm("Stats:\n Wins: X\n Ties: X\n Losses: X\n");
+
+prompt("Play again?")
+
+confirm("You Lose!");
+
+confirm("Stats:\n Wins: X\n Ties: X\n Losses: X\n");
+
+prompt("Play again?")
