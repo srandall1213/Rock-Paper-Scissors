9a66cab5343a1f30ba3ac5c0f84163ded13d8df6
Randie <srandall1213@gmail.com>
2022-06-24 14:10:37 -0600

    greeting,prompt,random generator done

--- script.js
+++ script.js
@@ -1,19 +1,53 @@
-confirm("Want to play Rock, Paper, Scissors?");
+//Greeting - DONE
 
-prompt("Please enter R, P, or S to signify your choice of rock, paper, or scissors.");
+var greeting = "Lets play Rock, Paper, Scissors!";
 
-alert("Opponent: R, P, or S"); //find out how to randomly pick
+alert(greeting);
 
-//You win scenario
-alert("You Win!"); //conditional if statement
+//User enters R, P, S - DONE
 
-alert("Stats:\n Wins: X\n Ties: X\n Losses: X\n");
+var instructions = "Please enter R, P, or S to signify your choice of rock, paper, or scissors.";
 
-confirm("Play again?") //loop back up to prompt
+prompt(instructions);
 
-//You lose scenario
-alert("You Lose!"); //conditional if statement
+//Randomly generated computer response - DONE
 
-alert("Stats:\n Wins: X\n Ties: X\n Losses: X\n");
+var opponentResponse = ["Rock", "Paper", "Scissors"];
 
-confirm("Play again?") //loop back up to prompt
+var pickOpponentResponse = opponentResponse[Math.floor(Math.random()*opponentResponse.length)];
+
+alert("Opponent's response: " + pickOpponentResponse); 
+
+//Conditional Loop Statement if user/opponent wins - START WORKING HERE!!!!
+
+var declareWin = "You win!";
+alert(win);
+
+var declareLose = "You lose!";
+alert(lose);
+
+var stats = "Stats:\n Wins: X\n Ties: X\n Losses: X\n";
+alert(stats);
+
+var again = "Play again?";
+confirm(again)
+
+
+
+
+//Instructions
+
+// As a user, I want to play Rock, Paper, Scissors against an automated opponent.
+
+// As a user, I can enter R, P, or S to signify my choice of rock, paper, or scissors.
+
+// As a user, I expect the computer to choose R, P, or S in return.
+
+// As a user, I want the option to play again whether I win or lose.
+
+// As a user, I want to see my total wins, ties, and losses after each round.
+
+// Specifications
+// Must use the alert(), confirm(), and prompt() methods to collect user input and display information to the user.
+
+// The computer's selection must be random to ensure a fair game.
